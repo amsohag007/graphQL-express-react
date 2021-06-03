8e8badf41dc67fa7b8a01bf037d7ec7cc55ea871
amsohag007 <amsohag007@gmail.com>
2021-06-03 15:28:01 +0600

    graphql express server setup,schema, models, and mutation

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://amsohag:amsohag@shop1.mnv2d.mongodb.net/myFirstGraphQL?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("listening to port 4000");
+});
